16aed62edeab6fbd6395bcadad9931e7ee9035cd
Qianjx <jq3513@rit.edu>
2020-04-18 16:41:50 -0400

    over

--- HW9_MQ.py
+++ HW9_MQ.py
@@ -62,19 +62,21 @@ def feature_selection_using_CC(CC_mat, data):
     using CC to select two features that has the greatest abs and has opposite sign
     using LDA to test Accuracy
     '''
-    print(CC_mat)
+    print("cross correlation matrix:\n" + str(CC_mat))
     # assign the feature by abs sort and choose the greates
     feature1 = CC_mat.abs().sort_values(ascending=False).index[0]
-    if CC_mat[feature1] < 0: feature2 = CC_mat.sort_values(ascending=False).index[0]
-    else: feature2 = CC_mat.sort_values(ascending=True).index[0]
-    print(feature1 + feature2)
+    if CC_mat[feature1] < 0: 
+        feature2 = CC_mat.sort_values(ascending=False).index[0]
+    else: 
+        feature2 = CC_mat.sort_values(ascending=True).index[0]
+    print(str(feature1) +' '+ str(feature2) + ' Selected')
     plot(data, feature1, feature2)
     
     # LDA classify the trainning data
     clf = LinearDiscriminantAnalysis(solver = 'eigen', n_components = 1)
     clf.fit(data.loc[: ,[feature1, feature2]], data['Class'])
     curr_score = clf.score(data.loc[: ,[feature1, feature2]], data['Class'])
-    print(round(curr_score, 3))
+    print("Accuracy for these features:"+ str(round(curr_score, 3)))
 
 def BFS_analysis(data):
     '''
@@ -84,6 +86,7 @@ def BFS_analysis(data):
     score_second = 0
     for feature1 in data.columns:
         for feature2 in data.columns:
+            # Get all combination of feature pairs.
             if feature1 != feature2 and feature1 != 'Class' and feature2 != 'Class':
                 clf = LinearDiscriminantAnalysis(solver = 'eigen', n_components = 1)
                 clf.fit(data.loc[: ,[feature1, feature2]], data['Class'])
@@ -99,19 +102,22 @@ def PCA_analysis(data):
     '''
     Using PCA to analyze data
     '''
+    '''
     # print Covariance Matrix
     X_std = feature_generation(data.loc[:, data.columns != 'Class'])
     mean_vec = np.mean(X_std, axis=0)
     cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
     print('Covariance matrix \n%s' %cov_mat)
-
-
-    pca = PCA(n_components = 2)
+    '''
+    pca = PCA(n_components = 3)
     pca.fit(data.loc[:, data.columns != 'Class'])
-    print(pca.components_)
-    print(pca.singular_values_)
-    data['PCA_1'] = data.loc[:, data.columns != 'Class'].dot(pca.components_[0])
-    data['PCA_2'] = data.drop(['Class','PCA_1'], axis = 1).dot(pca.components_[1])
+    print("eigen vectors of PCA\n"+str(pca.components_))
+    print("singular values of PCA\n"+str(pca.singular_values_))
+    print('We will use the first 2 eigen vectors')
+
+    # doing projection
+    data['PCA_feature1'] = data.loc[:, data.columns != 'Class'].dot(pca.components_[0])
+    data['PCA_feature2'] = data.drop(['Class','PCA_feature1'], axis = 1).dot(pca.components_[1])
     
     return data
 
@@ -120,9 +126,6 @@ def plot(data, feature1, feature2):
     '''
     General plotting method
     '''
-
-    plt.xlim(0,10)
-    plt.ylim(0,10)
 
     plt.xlabel(feature1)
     plt.ylabel(feature2)
@@ -136,6 +139,8 @@ def plot(data, feature1, feature2):
         edgecolors='b'
     )
 
+    '''
+    # This part is for plotting decision boundry
     CA = np.mean(data.loc[data['Class'] == 1,[feature1, feature2]])
     CB = np.mean(data.loc[data['Class'] == -1,[feature1, feature2]])
     plt.plot([CA[0],CB[0]],[CA[1],CB[1]],\
@@ -144,6 +149,8 @@ def plot(data, feature1, feature2):
     plt.plot(x, -(CA[0]-CB[0])/(CA[1]-CB[1]+0.)*(x-(CA[0]+CB[0])/2.)+(CA[1]+CB[1])/2. ,\
          'k-', label='decision boundry')
     # formula for Vertical line
+    '''
+
     plt.show()
 
 def main():
@@ -151,31 +158,30 @@ def main():
     # read csv file
     abominable_data = pd.read_csv('Abominable_Data_HW19_v420.csv')
     test_data = pd.read_csv('Abominable_UNCLASSIFIED_Data_HW19_v420.csv')
-    print(test_data)
     data = feature_generation(balancing_data(abominable_data))
-    # print("After processing, data.head:\n"+ str(data.head()))
     
-    # print(CC_analysis(data))
+    print("1. Cross Correlation part:")
     # feature selection
     feature_selection_using_CC(CC_analysis(data), data)
+
+    print('\n2. LDA part:')
     # cross correlation analysis part
     features, score, best_clf, score_second = BFS_analysis(data)
-    # [issue]tempary print out the score as the accuracy
-    print(str(features) + ' ' + str(round(score, 3)) + ' ' + str(round(score_second, 3)))
+    print('These features are selected:' + str(features) +\
+         '\nHighest and Second Highest Accuracy is '\
+         + str(round(score, 3)) + ' ' + str(round(score_second, 3)))
     plot(data, features[0], features[1])
     
-    # test the classifier on unclassified test data
-    # test_result= best_clf.predict(feature_generation(balancing_data(test_data[features])))
-    test_result= best_clf.predict(feature_generation(test_data)[features])
+    print('\n3. PCA part:')
+    # PCA part
+    # new data is input data after projection on PCA
+    new_data = PCA_analysis(data)
+    plot(new_data, 'PCA_feature1', 'PCA_feature2')
     
-    # brute force search part
-
-    new_data = PCA_analysis(balancing_data(abominable_data))
-
-    plot(new_data, 'PCA_1', 'PCA_2')
-  
-    # doing some preprocessing on the dataset
-    #data = feature_generation(balancing_data(abominable_data))
+    # test the classifier on unclassified test data
+    test_result= best_clf.predict(feature_generation(test_data)[features])
+    pd.DataFrame(test_result).to_csv('HW_09_Classiﬁed_Results.csv')
+    print('test result generated')
 
 if __name__ == "__main__":
     main()
